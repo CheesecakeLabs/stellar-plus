e584e31d8cb12451000dca9d8dca747f596f32d9
Fabricius Zatti <62725221+fazzatti@users.noreply.github.com>
2023-12-11 10:22:19 -0300

    fix account handler types

--- src/stellar-plus/account/account-handler/types.d.ts
+++ src/stellar-plus/account/account-handler/types.d.ts
@@ -1,6 +1,8 @@
+import { Transaction } from '@stellar/stellar-sdk'
+
 import { AccountBase } from '@account/base/types'
 import { AccountHelpersPayload } from '@account/helpers/types'
-import { Transaction, TransactionXdr } from '@stellar-plus/types'
+import { TransactionXdr } from '@stellar-plus/types'
 
 export type AccountHandler = AccountBase & {
   sign(tx: Transaction): Promise<TransactionXdr> | TransactionXdr
