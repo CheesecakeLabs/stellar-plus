3c3b978bdcdc52ebdce6a226c13e20333a26a6c8
Fabricius Zatti <62725221+fazzatti@users.noreply.github.com>
2023-12-11 09:47:24 -0300

    soroban docstring

--- src/stellar-plus/soroban/index.ts
+++ src/stellar-plus/soroban/index.ts
@@ -6,6 +6,13 @@ export class SorobanHandlerClient implements SorobanHandler {
   private network: Network
   public server: SorobanRpcServer
 
+  /**
+   *
+   * @param {Network} network - The network to use.
+   *
+   * @description - The soroban handler is used for interacting with the Soroban server.
+   *
+   */
   constructor(network: Network) {
     this.network = network
     this.server = new Server(this.network.horizonUrl)
