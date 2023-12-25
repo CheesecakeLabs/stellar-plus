e94ba171f714661702e93f65f5cb8bccac6f1fdd
Fabricius Zatti <62725221+fazzatti@users.noreply.github.com>
2023-12-25 16:10:17 -0300

    test: fix default account handler secret key test

--- src/stellar-plus/account/account-handler/default/default.test.ts
+++ src/stellar-plus/account/account-handler/default/default.test.ts
@@ -13,6 +13,8 @@ describe('DefaultAccountHandler', () => {
   let mockedUnsignedTx: Transaction
   let mockedSignedTxXdr: string
 
+  const mockedNetwork = Constants.testnet
+
   beforeEach(() => {
     mockedKeypair = mockedSimpleKeyPair
     mockedUnsignedTx = mockUnsignedClassicTransaction
@@ -22,18 +24,18 @@ describe('DefaultAccountHandler', () => {
   it('should create an instance with a provided secret key', () => {
     const secretKey = mockedKeypair.secretKey
     const client = new DefaultAccountHandler({ secretKey })
-    expect(client.secretKey).toBe(secretKey)
+    expect(client.getPublicKey()).toBe(mockedKeypair.publicKey)
   })
 
   it('should create an instance with a random secret key', () => {
-    const client = new DefaultAccountHandler({ network: Constants.testnet })
-    // Check if the secret key starts with 'S'
-    expect(client.secretKey).toMatch(/^S/)
-    // Check the length of the secret key (assuming the length is known, e.g., 56)
-    expect(client.secretKey).toHaveLength(56)
+    const client = new DefaultAccountHandler({ network: mockedNetwork })
+    // Check if the public key starts with 'G'
+    expect(client.getPublicKey()).toMatch(/^G/)
+    // Check the length of the public key (assuming the length is known, e.g., 56)
+    expect(client.getPublicKey()).toHaveLength(56)
     // Use a regular expression to validate the format (this regex is just an example)
-    const secretKeyRegex = /^S[A-Z2-7]{55}$/
-    expect(client.secretKey).toMatch(secretKeyRegex)
+    const publicKeyRegex = /^G[A-Z2-7]{55}$/
+    expect(client.getPublicKey()).toMatch(publicKeyRegex)
   })
 
   it('should return the public key of the account', () => {
