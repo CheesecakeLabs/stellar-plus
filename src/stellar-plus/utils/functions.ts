3941e958b5c280d6b01fc351ead4a639b2476334
Fabricius Zatti <62725221+fazzatti@users.noreply.github.com>
2023-12-27 14:02:42 -0300

    build: remove crypto dependency

---
+++ src/stellar-plus/utils/functions.ts
@@ -0,0 +1,11 @@
+export const generateRandomSalt = (): Buffer => {
+  let salt = ''
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' // Base64 characters
+  const charactersLength = characters.length
+
+  for (let i = 0; i < 32; i++) {
+    salt += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+
+  return Buffer.from(salt)
+}
