0bad0bf212680bee2664d772bbfb8e8e88b9742a
Fabricius Zatti <62725221+fazzatti@users.noreply.github.com>
2024-01-02 15:09:36 -0300

    feat: add exports to main types

--- src/stellar-plus/account/index.ts
+++ src/stellar-plus/account/index.ts
@@ -1,12 +1,19 @@
 import { DefaultAccountHandlerClient as DefaultAccountHandler } from 'stellar-plus/account/account-handler/default'
 import { FreighterAccountHandlerClient as FreighterAccountHandler } from 'stellar-plus/account/account-handler/freighter'
+import { AccountHandler } from 'stellar-plus/account/account-handler/types'
 import { AccountBaseClient as Base } from 'stellar-plus/account/base'
+import { AccountBase } from 'stellar-plus/account/base/types'
 
 //
 // Export Classes
 //
 
 export { Base, FreighterAccountHandler, DefaultAccountHandler }
+
+export type BaseAccountType = AccountBase
+export type AccountHandlerType = AccountHandler
+export type FreighterAccountHandlerType = typeof FreighterAccountHandler
+export type DefaultAccountHandlerType = typeof DefaultAccountHandler
 
 //
 // export types
