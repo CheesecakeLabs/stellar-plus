7a8f6dab3d6a28eb860c01ac11acd0f08cccbf47
Fabricius Zatti <62725221+fazzatti@users.noreply.github.com>
2024-01-24 17:21:35 -0300

    feat: add fee bump plugin

---
+++ src/stellar-plus/utils/pipeline/plugins/fee-bump/index.ts
@@ -0,0 +1,66 @@
+import { FeeBumpTransaction, Transaction } from '@stellar/stellar-sdk'
+
+import { ClassicSignRequirementsPipeline } from 'stellar-plus/core/pipelines/classic-sign-requirements'
+import { FeeBumpPipeline } from 'stellar-plus/core/pipelines/fee-bump'
+import { SignTransactionPipeline } from 'stellar-plus/core/pipelines/sign-transaction'
+import {
+  SubmitTransactionPipelineInput,
+  SubmitTransactionPipelineOutput,
+  SubmitTransactionPipelineType,
+} from 'stellar-plus/core/pipelines/submit-transaction/types'
+import { FeeBumpHeader } from 'stellar-plus/types'
+import { BeltMetadata, BeltPluginType } from 'stellar-plus/utils/pipeline/conveyor-belts/types'
+
+export class FeeBumpWrapperPlugin
+  implements
+    BeltPluginType<SubmitTransactionPipelineInput, SubmitTransactionPipelineOutput, SubmitTransactionPipelineType>
+{
+  readonly type: SubmitTransactionPipelineType = 'SubmitTransactionPipeline'
+  readonly name: string = 'FeeBumpWrapperPlugin'
+
+  private feeBumpHeader: FeeBumpHeader
+
+  constructor(feeBumpHeader: FeeBumpHeader) {
+    this.feeBumpHeader = feeBumpHeader
+  }
+
+  public async preProcess(
+    item: SubmitTransactionPipelineInput,
+    meta: BeltMetadata
+  ): Promise<SubmitTransactionPipelineInput> {
+    const { itemId } = meta
+    const { transaction }: SubmitTransactionPipelineInput = item
+
+    if (transaction instanceof FeeBumpTransaction) {
+      throw new Error('Transaction is already a FeeBump, FeeBumpWrapperPlugin should not be used')
+    }
+
+    const feeBumpPipeline = new FeeBumpPipeline()
+    const feeBumpEnvelope = await feeBumpPipeline.execute(
+      {
+        innerTransaction: transaction as Transaction,
+        feeBumpHeader: this.feeBumpHeader,
+      },
+      itemId
+    )
+
+    const classicSignRequirementsPipeline = new ClassicSignRequirementsPipeline()
+    const classicSignRequirements = await classicSignRequirementsPipeline.execute(feeBumpEnvelope, itemId)
+    console.log('classicSignRequirements', classicSignRequirements)
+    const signTransactionPipeline = new SignTransactionPipeline()
+    const signedTransaction = await signTransactionPipeline.execute(
+      {
+        transaction: feeBumpEnvelope,
+        signatureRequirements: classicSignRequirements,
+        signers: this.feeBumpHeader.signers,
+      },
+      itemId
+    )
+
+    const updatedItem: SubmitTransactionPipelineInput = {
+      ...item,
+      transaction: signedTransaction,
+    }
+    return updatedItem
+  }
+}
