2de9d3ebf3826d8881edad49c268deca3cbf116b
Fabricius Zatti <62725221+fazzatti@users.noreply.github.com>
2024-02-02 10:53:51 -0300

    fix: Fee bump wrapper will check for inner fee when same source to decide upon wrapping

--- src/stellar-plus/utils/pipeline/plugins/submit-transaction/fee-bump/index.ts
+++ src/stellar-plus/utils/pipeline/plugins/submit-transaction/fee-bump/index.ts
@@ -35,8 +35,19 @@ export class FeeBumpWrapperPlugin
       throw new Error('Transaction is already a FeeBump, FeeBumpWrapperPlugin should not be used')
     }
 
-    // No fee bump is applied if source is the same for the fee bump and the transaction
-    if ((transaction as Transaction).source === this.feeBumpHeader.header.source) {
+    //
+    //
+    // No fee bump is applied if both conditions are met:
+    //      - source is the same for the fee bump and the transaction
+    //      - fee in the original transaction is equal or higher than the fee in the fee bump
+    //
+    // A Fee bump for the same source account is only effective if the fee is increased in the process.
+    // A lower or equal fee will result in errors during processing.
+    //
+    if (
+      (transaction as Transaction).source === this.feeBumpHeader.header.source &&
+      (transaction as Transaction).fee >= this.feeBumpHeader.header.fee
+    ) {
       return item
     }
 
