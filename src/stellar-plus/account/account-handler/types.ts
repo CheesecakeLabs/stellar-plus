d2361ad646ea66102be3fe290f51477e4bb447f9
Fabricius Zatti <62725221+fazzatti@users.noreply.github.com>
2024-02-09 14:26:52 -0300

    feat: add default signing for aoroban auth

--- src/stellar-plus/account/account-handler/types.ts
+++ src/stellar-plus/account/account-handler/types.ts
@@ -1,4 +1,4 @@
-import { FeeBumpTransaction, Transaction } from '@stellar/stellar-sdk'
+import { FeeBumpTransaction, Transaction, xdr } from '@stellar/stellar-sdk'
 
 import { AccountBase } from 'stellar-plus/account/base/types'
 import { AccountHelpersPayload } from 'stellar-plus/account/helpers/types'
@@ -7,6 +7,11 @@ import { TransactionXdr } from 'stellar-plus/types'
 export type AccountHandler = AccountBase & {
   getPublicKey(): string
   sign(tx: Transaction | FeeBumpTransaction): Promise<TransactionXdr> | TransactionXdr
+  signSorobanAuthEntry(
+    entry: xdr.SorobanAuthorizationEntry,
+    validUntilLedgerSeq: number,
+    networkPassphrase: string
+  ): Promise<xdr.SorobanAuthorizationEntry>
   signatureSchema?: SignatureSchema
 }
 
