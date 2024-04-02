c7d5419c29e55360d4df88869097390514b74ab0
Fabricius Zatti <62725221+fazzatti@users.noreply.github.com>
2024-04-02 10:42:04 -0300

    test: complete coverage for simulate transaction pipeline

--- src/stellar-plus/core/pipelines/simulate-transaction/index.unit.test.ts
+++ src/stellar-plus/core/pipelines/simulate-transaction/index.unit.test.ts
@@ -1,45 +1,50 @@
-import { Account, TransactionBuilder } from '@stellar/stellar-base'
-import { SorobanRpc } from '@stellar/stellar-sdk'
+import { Account, SorobanDataBuilder, SorobanRpc, TransactionBuilder, xdr } from '@stellar/stellar-sdk'
 
 import { Constants } from 'stellar-plus'
 import { SimulateTransactionPipeline } from 'stellar-plus/core/pipelines/simulate-transaction'
 import { ConveyorBeltErrorMeta } from 'stellar-plus/error/helpers/conveyor-belt'
-import { DefaultRpcHandler } from 'stellar-plus/rpc/default-handler'
 import { RpcHandler } from 'stellar-plus/rpc/types'
-import { NetworkConfig } from 'stellar-plus/types'
 import { BeltMetadata } from 'stellar-plus/utils/pipeline/conveyor-belts/types'
 
 import { PSIError } from './errors'
 import { SimulateTransactionPipelineInput, SimulateTransactionPipelineType } from './types'
 
-jest.mock('stellar-plus/rpc/default-handler', () => ({
-  DefaultRpcHandler: jest.fn().mockImplementation(() => {
-    return {
-      type: 'RpcHandler',
-      networkConfig: {} as NetworkConfig,
-      server: jest.fn(),
-      getTransaction: jest.fn(),
-      getLatestLedger: jest.fn(),
-      getHealth: jest.fn(),
-      getNetwork: jest.fn(),
-      getEvents: jest.fn(),
-      getLedgerEntries: jest.fn(),
-      simulateTransaction: jest.fn(),
-      prepareTransaction: jest.fn(),
-      submitTransaction: jest.fn(),
-    }
-  }),
-}))
-
-const MOCKED_RPC_HANDLER = DefaultRpcHandler as jest.MockedClass<typeof DefaultRpcHandler>
-
-const MOCKED_SIMULATION_RESPONSE_ERROR = {
-  error: 'mocked error',
+const MOCKED_SIMULATION_RESPONSE_BASE = {
   events: [],
   id: 'mocked-id',
   latestLedger: 0,
   _parsed: true,
+}
+const MOCKED_SIMULATION_RESPONSE_ERROR = {
+  ...MOCKED_SIMULATION_RESPONSE_BASE,
+  error: 'mocked error',
 } as SorobanRpc.Api.SimulateTransactionErrorResponse
+
+const MOCKED_SIMULATION_RESPONSE_SUCCESS = {
+  ...MOCKED_SIMULATION_RESPONSE_BASE,
+  transactionData: new SorobanDataBuilder(),
+  minResourceFee: '0',
+  cost: {
+    cpuInsns: '0',
+    memBytes: '0',
+  },
+} as SorobanRpc.Api.SimulateTransactionSuccessResponse
+
+const MOCKED_SIMULATION_RESPONSE_RESTORE = {
+  ...MOCKED_SIMULATION_RESPONSE_SUCCESS,
+  result: {
+    auth: [],
+    xdr: 'mocked-xdr',
+    retval: xdr.ScVal.scvVoid(),
+  },
+  restorePreamble: {
+    minResourceFee: '0',
+    transactionData: new SorobanDataBuilder(),
+  },
+} as SorobanRpc.Api.SimulateTransactionRestoreResponse
+
+const MOCKED_INVALID_SIMULATION_RESPONSE =
+  MOCKED_SIMULATION_RESPONSE_BASE as unknown as SorobanRpc.Api.SimulateTransactionResponse
 
 const MOCKED_PK_A = 'GACF23GKVFTU77K6W6PWSVN7YBM63UHDULILIEXJO6FR4YKMJ7FW3DTI'
 // const MOCKED_PK_B = 'GB3MXH633VRECLZRUAR3QCLQJDMXNYNHKZCO6FJEWXVWSUEIS7NU376P'
@@ -60,6 +65,9 @@ const MOCKED_TX_OPTIONS: TransactionBuilder.TransactionBuilderOptions = {
   },
 }
 
+const MOCKED_TRANSACTION = new TransactionBuilder(MOCKED_ACCOUNT_A, MOCKED_TX_OPTIONS).build()
+const MOCKED_EXCEPTION = new Error('simulateTransaction failed')
+
 const mockConveyorBeltErrorMeta = (
   item: SimulateTransactionPipelineInput
 ): ConveyorBeltErrorMeta<SimulateTransactionPipelineInput, BeltMetadata> => {
@@ -74,26 +82,28 @@ const mockConveyorBeltErrorMeta = (
 }
 
 describe('SimulateTransactionPipeline', () => {
-  let pipeline: SimulateTransactionPipeline
   beforeEach(() => {
-    pipeline = new SimulateTransactionPipeline()
     jest.clearAllMocks()
   })
 
   describe('Initialization', () => {
     it('should initialize the pipeline successfully', async () => {
+      const pipeline = new SimulateTransactionPipeline()
       expect(pipeline).toBeInstanceOf(SimulateTransactionPipeline)
     })
   })
 
   describe('errors', () => {
     it('should throw failedToSimulateTransaction error when simulateTransaction fails to perform the simulation', async () => {
-      const MOCKED_EXCEPTION = new Error('simulateTransaction failed')
-      MOCKED_RPC_HANDLER.prototype.simulateTransaction = jest.fn().mockRejectedValueOnce(MOCKED_EXCEPTION)
-      pipeline = new SimulateTransactionPipeline()
+      const MOCKED_RPC = {
+        simulateTransaction: jest.fn().mockImplementation(() => {
+          throw MOCKED_EXCEPTION
+        }),
+      } as unknown as RpcHandler
+      const pipeline = new SimulateTransactionPipeline()
       const item = {
-        transaction: new TransactionBuilder(MOCKED_ACCOUNT_A, MOCKED_TX_OPTIONS).build(),
-        rpcHandler: MOCKED_RPC_HANDLER as unknown as RpcHandler,
+        transaction: MOCKED_TRANSACTION,
+        rpcHandler: MOCKED_RPC,
       }
 
       await expect(pipeline.execute(item)).rejects.toThrow(
@@ -102,18 +112,99 @@ describe('SimulateTransactionPipeline', () => {
     })
 
     it('should throw simulationFailed error when simulateTransaction provides a simulation that will fail to be executed', async () => {
-      MOCKED_RPC_HANDLER.prototype.simulateTransaction = jest
-        .fn()
-        .mockResolvedValueOnce(MOCKED_SIMULATION_RESPONSE_ERROR)
-      pipeline = new SimulateTransactionPipeline()
+      const MOCKED_RPC = {
+        simulateTransaction: jest.fn().mockImplementation(() => {
+          return MOCKED_SIMULATION_RESPONSE_ERROR
+        }),
+      } as unknown as RpcHandler
+
+      const pipeline = new SimulateTransactionPipeline()
       const item = {
-        transaction: new TransactionBuilder(MOCKED_ACCOUNT_A, MOCKED_TX_OPTIONS).build(),
-        rpcHandler: MOCKED_RPC_HANDLER as unknown as RpcHandler,
+        transaction: MOCKED_TRANSACTION,
+        rpcHandler: MOCKED_RPC,
       }
 
       await expect(pipeline.execute(item)).rejects.toThrow(
         PSIError.simulationFailed(MOCKED_SIMULATION_RESPONSE_ERROR, mockConveyorBeltErrorMeta(item))
       )
+    })
+
+    it('should throw simulationResultCouldNotBeVerified error when simulateTransaction provides a simulation that cannot be verified', async () => {
+      const MOCKED_RPC = {
+        simulateTransaction: jest.fn().mockImplementation(() => {
+          return MOCKED_INVALID_SIMULATION_RESPONSE
+        }),
+      } as unknown as RpcHandler
+      const pipeline = new SimulateTransactionPipeline()
+      const item = {
+        transaction: MOCKED_TRANSACTION,
+        rpcHandler: MOCKED_RPC,
+      }
+
+      await expect(pipeline.execute(item)).rejects.toThrow(
+        PSIError.simulationResultCouldNotBeVerified(MOCKED_INVALID_SIMULATION_RESPONSE, mockConveyorBeltErrorMeta(item))
+      )
+    })
+
+    it('should throw failedToAssembleTransaction error when assembleTransaction fails to assemble the transaction', async () => {
+      const MOCKED_RPC = {
+        simulateTransaction: jest.fn().mockImplementation(() => {
+          return MOCKED_SIMULATION_RESPONSE_SUCCESS
+        }),
+      } as unknown as RpcHandler
+      const pipeline = new SimulateTransactionPipeline()
+      const item = {
+        transaction: MOCKED_TRANSACTION,
+        rpcHandler: MOCKED_RPC,
+      }
+
+      await expect(pipeline.execute(item)).rejects.toThrow('Failed to assemble transaction!')
+    })
+  })
+
+  describe('success', () => {
+    it('should return a successful response when simulateTransaction provides a simulation that can be verified as successfull', async () => {
+      const MOCKED_RPC = {
+        simulateTransaction: jest.fn().mockImplementation(() => {
+          return MOCKED_SIMULATION_RESPONSE_SUCCESS
+        }),
+      } as unknown as RpcHandler
+      const pipeline = new SimulateTransactionPipeline()
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      jest.spyOn(pipeline as any, 'assembleTransaction').mockImplementation(() => {
+        return MOCKED_TRANSACTION
+      })
+      const item = {
+        transaction: MOCKED_TRANSACTION,
+        rpcHandler: MOCKED_RPC,
+      }
+
+      expect(await pipeline.execute(item)).toEqual({
+        response: MOCKED_SIMULATION_RESPONSE_SUCCESS,
+        assembledTransaction: MOCKED_TRANSACTION,
+      })
+    })
+
+    it('should return a successful response when simulateTransaction provides a simulation that can be verified as successfull but needs to be restored', async () => {
+      const MOCKED_RPC = {
+        simulateTransaction: jest.fn().mockImplementation(() => {
+          return MOCKED_SIMULATION_RESPONSE_RESTORE
+        }),
+      } as unknown as RpcHandler
+      const pipeline = new SimulateTransactionPipeline()
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      jest.spyOn(pipeline as any, 'assembleTransaction').mockImplementation(() => {
+        return MOCKED_TRANSACTION
+      })
+      const item = {
+        transaction: MOCKED_TRANSACTION,
+        rpcHandler: MOCKED_RPC,
+      }
+
+      expect(await pipeline.execute(item)).toEqual({
+        response: MOCKED_SIMULATION_RESPONSE_RESTORE,
+        assembledTransaction: MOCKED_TRANSACTION,
+      })
     })
   })
 })
