634cab7d794babbdb7c4f884a962ecd0c42978d9
Fabricius Zatti <fazzatti@gmail.com>
2024-06-28 08:59:18 -0300

    test: add simple tx invocation helper

--- src/tests/utils/index.ts
+++ src/tests/utils/index.ts
@@ -1,4 +1,7 @@
+import { TransactionI } from '@stellar/stellar-sdk'
 import { readFile } from 'fs/promises'
+import { AccountHandler, DefaultAccountHandler } from 'stellar-plus/account'
+import { TransactionInvocation } from 'stellar-plus/types'
 
 export const loadWasmFile = async (wasmFilePath: string): Promise<Buffer> => {
   try {
@@ -7,5 +10,16 @@ export const loadWasmFile = async (wasmFilePath: string): Promise<Buffer> => {
   } catch (error) {
     console.error(`Error reading the WASM file: ${error}`)
     throw error
+  }
+}
+
+export const simpleTxInvocation = (account: AccountHandler): TransactionInvocation => {
+  return {
+    header: {
+      source: account.getPublicKey(),
+      fee: '100000',
+      timeout: 0,
+    },
+    signers: [account],
   }
 }
