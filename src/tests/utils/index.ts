61cba058eafb60e5c74d06088c52cd72c8df05d7
Fabricius Zatti <fazzatti@gmail.com>
2024-06-29 16:27:51 -0300

    test: add util to derivate feebump header from tx invocation

--- src/tests/utils/index.ts
+++ src/tests/utils/index.ts
@@ -1,7 +1,7 @@
 import { TransactionI } from '@stellar/stellar-sdk'
 import { readFile } from 'fs/promises'
 import { AccountHandler, DefaultAccountHandler } from 'stellar-plus/account'
-import { TransactionInvocation } from 'stellar-plus/types'
+import { FeeBumpHeader, TransactionInvocation } from 'stellar-plus/types'
 
 export const loadWasmFile = async (wasmFilePath: string): Promise<Buffer> => {
   try {
@@ -17,9 +17,19 @@ export const simpleTxInvocation = (account: AccountHandler): TransactionInvocati
   return {
     header: {
       source: account.getPublicKey(),
-      fee: '100000',
+      fee: '10000000',
       timeout: 0,
     },
     signers: [account],
   }
+}
+
+export const simpleTxInvocationToFeebump = (txInvocation: TransactionInvocation): FeeBumpHeader => {
+  return {
+    ...txInvocation,
+    header: {
+      ...txInvocation.header,
+      fee: (Number(txInvocation.header.fee) * 10).toString(), // Ensure the feebump fee is larger
+    },
+  } as FeeBumpHeader
 }
